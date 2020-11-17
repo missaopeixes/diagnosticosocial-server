ec6a21787bebb8717e5b10ab5947d8f8356cf60f
Vitor Hugo <vhnogueiranf@gmail.com>
2020-11-17 18:04:41 -0300

    Enviando contrato mais amplo na autenticação.

---
+++ src/modules/auth/auth-model.ts
@@ -0,0 +1,16 @@
+
+export class Auth {
+
+  id: number = 0
+  login: string = '';
+  nome: string = ''
+  administrador: boolean = false
+  validade: string = '1d'
+  token: string = '';
+
+  constructor(obj?: Partial<Auth>) {
+    if (obj) {
+      Object.assign(this, obj);
+    }
+  }
+}
